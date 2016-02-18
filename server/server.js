b3a211003b26b902e1351e11087b029a33790495
Jisoo Yoon <jyoon006@gmail.com>
2016-02-18 13:54:14 -0500

    (feat) Connect to local mongoose database, create new static server to client

--- server/server.js
+++ server/server.js
@@ -4,9 +4,19 @@ var mongoose = require('mongoose');
 var app = express();
 var port = process.env.PORT || 8000;
 
+
 module.exports = app;
 
 var yelpRoutes = require('./yelp/yelpRoutes.js');
+
+mongoose.connect('mongodb://localhost/brachiosaurus');
+
+app.use(express.static(__dirname + '/../../client'));
+
+app.get('/*', function(req, res) {
+  res.end('hello world');
+});
+
 
 app.listen(port, function() {
   console.log('Listening on port ' + port);
