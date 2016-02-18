ae17ffba7a9b482a0928a0ed59a8d2e821965315
Jisoo Yoon <jyoon006@gmail.com>
2016-02-18 18:49:37 -0500

    (refactor) add mongodb connection

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ var mongoose = require('mongoose');
 
 var app = express();
 var port = process.env.PORT || 8000;
-
-
 module.exports = app;
+
+var eventRoute = require('./events/eventRoutes.js');
 
 var yelpRoutes = require('./yelp/yelpRoutes.js');
 
@@ -13,7 +13,7 @@ mongoose.connect('mongodb://localhost/brachiosaurus');
 
 app.use(express.static(__dirname + '/../../client'));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.end('hello world');
 });
 
@@ -21,3 +21,8 @@ app.get('/*', function(req, res) {
 app.listen(port, function() {
   console.log('Listening on port ' + port);
 });
+
+
+
+
+
