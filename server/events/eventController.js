2c62d640528b4164e8da33e2a2dc27e3d70bd72a
Jisoo Yoon <jyoon006@gmail.com>
2016-02-18 18:47:11 -0500

    (refactor) create new event-id and check the db to create or send back existing event-id data

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -1,25 +1,29 @@
+var randomWords = require('random-words');
 var Event = require('./eventModel.js');
 
 module.exports = {
   newEvent: function(req, res) {
     var event_name = req.body.event_name;
     var location = req.body.location;
-    var radius_filter = req.body.radius_filter;
+    var event_id = randomWords({exactly: 2}).join("");
 
-    Event.findOne({event_id: event_id}, function(err, event) { //check to see if event name exists
-        //create new event if event doesn't exist
+    Event.findOne({event_id: event_id}, function(err, event) { //check to see if event id exists
+      if(err) {
+        return console.error(err);
+      }      
+      //create new event if event doesn't exist
       if(!event) {
         Event.create({
+          event_id: event_id,
           event_name: event_name,
-          location: location,
-          radius_filter: radius_filter
+          location: location
         }, function(err, event) {
           if(err) {
             return console.error(err);
           }
-          res.json(event);  
+          res.json(event);  //send newly created event object to client
         });
-      } else {
+      } else { //if event id already exist, send client that event object
         res.json(event);
       }
     });
