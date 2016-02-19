4bff55133270e068327abeef0cd1dabae3ea33d9
Justin Bitely <jbitely@gmail.com>
2016-02-18 21:18:53 -0500

    (feat) Setup express route for '/', skeleton event view, controller

--- client/app/app.js
+++ client/app/app.js
@@ -1,13 +1,19 @@
 angular.module('clever', [
   'clever.choices',
+  'clever.event',
   'ngMaterial',
   'ngRoute'
 ])
-.config(function($routeProvider, $httpProvider){
+.config(function($locationProvider, $routeProvider, $httpProvider){
   $routeProvider
+    .when('/', {
+      templateUrl: 'app/event/event.html',
+      controller: 'EventController'
+    })
     .when('/choices', {
       templateUrl : 'app/choices/choices.html',
       controller: 'ChoicesController'
     });
 
+  $locationProvider.html5Mode(true);
 });
