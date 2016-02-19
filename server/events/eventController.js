8e5d62b936d3e9a85d53ea03006bda086e6c43da
Jisoo Yoon <jyoon006@gmail.com>
2016-02-18 20:37:26 -0500

    (refactor) add getEvent method to eventController, minor change in schema for EventModel, added routes for getEvent

--- server/events/eventController.js
+++ server/events/eventController.js
@@ -5,7 +5,9 @@ module.exports = {
   newEvent: function(req, res) {
     var event_name = req.body.event_name;
     var location = req.body.location;
-    var event_id = randomWords({exactly: 2}).join("");
+    var event_id = randomWords({exactly: 2}).join(""); //generate two random word to make it as event_id
+
+    
 
     Event.findOne({event_id: event_id}, function(err, event) { //check to see if event id exists
       if(err) {
@@ -23,10 +25,20 @@ module.exports = {
           }
           res.json(event);  //send newly created event object to client
         });
-      } else { //if event id already exist, send client that event object
-        res.json(event);
+      } else { //if randomly generated event_id already exist within db on create event call
+          return newEvent(req, res); //re-run function to get new event_id; 
       }
+    });
+  },
+  getEvent: function(req, res) {
+    //when user connects with events_id url that's created
+    var event_id = req.params.events_id;
+    //return data with same event_id
+    Events.findOne({event_id: event_id}, function(err, event) {
+      if(err) {
+        return console.error(err);
+      }
+      res.json(event);
     });
   }
 };
-
