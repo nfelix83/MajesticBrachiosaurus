83a450a04b309553d92c9e91ec64ff5bb0a4243e
Jisoo Yoon <jyoon006@gmail.com>
2016-02-19 18:19:29 -0500

    (feat) add sendEvent method for client to receive data specific to the event id

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -3,4 +3,6 @@ var app = require('../server.js');
 
 
 app.post('/create', eventController.newEvent);
+
 app.get('/:event_id', eventController.getEvent);
+app.post('/:event_id/details', eventController.sendEvent);
