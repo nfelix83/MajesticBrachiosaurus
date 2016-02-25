41b27ad2c2a2180d3febd892785c46f42310c93e
Jisoo Yoon <jyoon006@gmail.com>
2016-02-25 15:04:38 -0500

    (feat) add to database of new messages

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
-var http = require('http').Server(app)
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Event = require('./events/eventModel.js');
 
 
 var port = process.env.PORT || 8000;
-//connected to heroku
+//connect to heroku mongolab
 var uri = 'mongodb://testing:testing@ds013918.mongolab.com:13918/heroku_p92qhfjt' || 'mongodb://localhost/brachiosaurus';
 mongoose.connect(uri);
 
@@ -22,6 +23,22 @@ io.on('connection', function(socket){
 	//emit sends message out
 	console.log('a user connected');
 	socket.on('send:message', function (data) {
+    Event.findOne({event_id: data.event_id}, function(err, event) {
+      if(err) {
+        return console.error('Error finding same event id for chatroom',err);
+      }
+      //if event id is found, save new username/message into db
+      if(event) {
+        event.messages.username = data.name;
+        event.messages.message = data.message;
+      }
+
+      event.save(function(err) {
+        if(err) {
+          return console.error('Error in saving new chat messages', err);
+        }
+      });
+    });
       socket.broadcast.emit('send:message', {
         //user: name,
         text: data.message
